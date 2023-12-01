235856d838d3ad686e88d6bb1be9aa93c353a1a4
mc-gibyung <gb.chae@mcorpor.com>
2023-12-01 16:43:00 +0900

    init project

---
+++ action.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -eo pipefail
+
+# config
+repository=${REPOSITORY:-}
+format=${FORMAT:-}
+group=${GROUP:-}
+name=${NAME:-}
+version=${VERSION:-}
+nexus_host=${NEXUS_HOST:-}
+auth_base64=${AUTH_BASE64:-}
+
+setOutput() {
+    echo "${1}=${2}" >> "${GITHUB_OUTPUT}"
+}
+
+searchUrl="service/rest/v1/search?sort=version&direction=desc&repository=${repository}&format=${format}&group=${group}&name=${name}&version=${version}"
+
+# cURL
+response=$(curl --location "${nexus_host}/${searchUrl}" \
+--header "Authorization: Basic ${auth_base64}")
+
+# JSON parsing
+version=$(echo "$response" | jq '.items[0].version')
+
+echo "$version"
+
+if [ "$version" == "null" ]
+then
+  setOutput "is_component" "false"
+fi
+
+setOutput "is_component" "true"
